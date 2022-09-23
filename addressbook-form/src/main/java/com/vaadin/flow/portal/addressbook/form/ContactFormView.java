5b286d5cace4041aa4db27641fc9170b154c91d3
Mikhail Shabarov <mikhail@vaadin.com>
2022-09-23 15:42:34 +0300

    chore: PortletHub issue for Apache Pluto

--- addressbook-form/src/main/java/com/vaadin/flow/portal/addressbook/form/ContactFormView.java
+++ addressbook-form/src/main/java/com/vaadin/flow/portal/addressbook/form/ContactFormView.java
@@ -16,218 +16,64 @@
 package com.vaadin.flow.portal.addressbook.form;
 
 import javax.portlet.PortletMode;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
+import javax.portlet.WindowState;
 
-import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.EmailField;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.portal.PortletView;
 import com.vaadin.flow.portal.PortletViewContext;
-import com.vaadin.flow.portal.addressbook.backend.Contact;
-import com.vaadin.flow.portal.addressbook.backend.ContactService;
-import com.vaadin.flow.portal.lifecycle.PortletEvent;
-import com.vaadin.flow.portal.lifecycle.PortletModeEvent;
-
-import static com.vaadin.flow.portal.addressbook.backend.PortletEventConstants.EVENT_CONTACT_SELECTED;
-import static com.vaadin.flow.portal.addressbook.backend.PortletEventConstants.EVENT_CONTACT_UPDATED;
-import static com.vaadin.flow.portal.addressbook.backend.PortletEventConstants.KEY_CONTACT_ID;
 
 /**
  * @author Vaadin Ltd
  */
 public class ContactFormView extends VerticalLayout implements PortletView {
 
-    private static final String ACTION_EDIT = "Edit";
-    private static final String ACTION_CREATE = "Create new";
-    private static final String ACTION_SAVE = "Save";
+    public static final String STATE_MAXIMIZE = "Maximize";
+    public static final String STATE_NORMALIZE = "Normalize";
+    public static final String MODE_VIEW = "To view mode";
+    public static final String MODE_EDIT = "To edit mode";
+    public static final String WINDOW_STATE_CHANGE = "window-state-change";
+    public static final String PORTLET_MODE_CHANGE = "portlet-mode-change";
 
-    private PortletViewContext portletViewContext;
+    private Button windowState, portletMode;
 
-    private Binder<Contact> binder;
-    private Contact contact;
-
-    private Button action;
-    private Button cancel;
-    private Button remove;
-    private Image image;
-
-    private ContactService service;
+    private PortletViewContext context;
 
     @Override
     public void onPortletViewContextInit(PortletViewContext context) {
-        this.portletViewContext = context;
-        context.addEventChangeListener(EVENT_CONTACT_SELECTED,
-                this::onContactSelected);
-        context.addPortletModeChangeListener(this::handlePortletModeChange);
-        init();
+        this.context = context;
+        windowState = new Button(WindowState.NORMAL.equals(context.getWindowState()) ? STATE_MAXIMIZE : STATE_NORMALIZE, event -> switchWindowState());
+        windowState.setId(WINDOW_STATE_CHANGE);
+
+        portletMode = new Button(PortletMode.EDIT.equals(context.getPortletMode()) ? MODE_VIEW : MODE_EDIT, event -> switchPortletMode());
+        portletMode.setId(PORTLET_MODE_CHANGE);
+
+        add(windowState, portletMode);
     }
 
-    private void onContactSelected(PortletEvent event) {
-        int contactId = Integer
-                .parseInt(event.getParameters().get(KEY_CONTACT_ID)[0]);
-        Optional<Contact> contact = getService().findById(contactId);
-        if (contact.isPresent()) {
-            this.contact = contact.get();
-            updateActionText();
-            binder.readBean(this.contact);
-            if (this.contact.getImage() != null) {
-                image.setSrc(this.contact.getImage());
-                image.setVisible(true);
-            }
-            remove.setVisible(true);
+    private void switchWindowState() {
+        if (WindowState.NORMAL.equals(context.getWindowState())) {
+            context.setWindowState(WindowState.MAXIMIZED);
+            windowState.setText(STATE_NORMALIZE);
+            Notification.show("Switching to state " + WindowState.MAXIMIZED);
+        } else if (WindowState.MAXIMIZED.equals(context.getWindowState())) {
+            context.setWindowState(WindowState.NORMAL);
+            windowState.setText(STATE_MAXIMIZE);
+            Notification.show("Switching to state " + WindowState.NORMAL);
+        }
+    }
+
+    private void switchPortletMode() {
+        if (PortletMode.EDIT.equals(context.getPortletMode())) {
+            context.setPortletMode(PortletMode.VIEW);
+            portletMode.setText(MODE_EDIT);
+            Notification.show("Switching to mode " + PortletMode.VIEW);
         } else {
-            clear();
+            context.setPortletMode(PortletMode.EDIT);
+            portletMode.setText(MODE_VIEW);
+            Notification.show("Switching to mode " + PortletMode.EDIT);
         }
-    }
-
-    private void handlePortletModeChange(PortletModeEvent event) {
-        binder.setReadOnly(event.isViewMode());
-        if (event.isViewMode()) {
-            action.setText(ACTION_EDIT);
-        } else {
-            action.setText(ACTION_SAVE);
-        }
-    }
-
-    private void fireUpdateEvent(Contact contact) {
-        Map<String, String> param = Collections
-                .singletonMap(KEY_CONTACT_ID, contact.getId().toString());
-
-        portletViewContext.fireEvent(EVENT_CONTACT_UPDATED, param);
-    }
-
-    private PortletMode getPortletMode() {
-        return portletViewContext.getPortletMode();
-    }
-
-    private void init() {
-        FormLayout formLayout = populateFormLayout();
-        setupButtons();
-
-        HorizontalLayout actionButtons = new HorizontalLayout(action, cancel,
-                remove);
-        add(formLayout, actionButtons);
-        setHorizontalComponentAlignment(Alignment.END, actionButtons);
-    }
-
-    private ContactService getService() {
-        if (service == null) {
-            service = new ContactService();
-        }
-        return service;
-    }
-
-    private FormLayout populateFormLayout() {
-        FormLayout formLayout = new FormLayout();
-        TextField firstName = new TextField();
-        formLayout.addFormItem(firstName, "First name");
-
-        TextField lastName = new TextField();
-        formLayout.addFormItem(lastName, "Last name");
-
-        TextField phone = new TextField();
-        formLayout.addFormItem(phone, "Phone number");
-
-        EmailField email = new EmailField();
-        formLayout.addFormItem(email, "Email");
-
-        DatePicker birthDate = new DatePicker();
-        formLayout.addFormItem(birthDate, "Birth date");
-
-        binder = new Binder<>(Contact.class);
-        binder.bind(firstName, "firstName");
-        binder.bind(lastName, "lastName");
-        binder.bind(email, "email");
-        binder.bind(phone, "phoneNumber");
-        binder.bind(birthDate, "birthDate");
-
-        // Set the state of form depending on portlet mode.
-        binder.setReadOnly(PortletMode.VIEW.equals(getPortletMode()));
-
-        image = new Image();
-        image.setMaxHeight("72px");
-        image.setMaxWidth("72px");
-        image.setVisible(false);
-        formLayout.add(image);
-        return formLayout;
-    }
-
-    private void setupButtons() {
-        action = new Button("action", event -> {
-            if (PortletMode.EDIT.equals(getPortletMode())) {
-                save();
-            } else {
-                portletViewContext.setPortletMode(PortletMode.EDIT);
-            }
-        });
-
-        cancel = new Button("Cancel", event -> cancel());
-        remove = new Button("Remove", event -> remove());
-        remove.setVisible(false);
-        updateActionText();
-    }
-
-    private void updateActionText() {
-        action.setText(PortletMode.EDIT.equals(getPortletMode()) ?
-                ACTION_SAVE :
-                contact == null ? ACTION_CREATE : ACTION_EDIT);
-    }
-
-    private void clear() {
-        contact = null;
-        cancel();
-    }
-
-    private void cancel() {
-        if (contact != null) {
-            image.setSrc(contact.getImage());
-            image.setVisible(true);
-        }
-        if (PortletMode.EDIT.equals(getPortletMode())) {
-            binder.readBean(contact);
-            portletViewContext.setPortletMode(PortletMode.VIEW);
-        } else {
-            contact = null;
-            binder.removeBean();
-            binder.getFields().forEach(HasValue::clear);
-            image.setVisible(false);
-            remove.setVisible(false);
-        }
-        updateActionText();
-    }
-
-    private void remove() {
-        if (contact != null) {
-            getService().remove(contact);
-            contact = null;
-            cancel();
-            portletViewContext.setPortletMode(PortletMode.VIEW);
-        }
-        updateActionText();
-    }
-
-    private void save() {
-        if (contact != null) {
-            binder.writeBeanIfValid(contact);
-            getService().save(contact);
-        } else {
-            contact = new Contact(getService().getNextId());
-            binder.writeBeanIfValid(contact);
-            getService().create(contact);
-        }
-        fireUpdateEvent(contact);
-        updateActionText();
-
-        portletViewContext.setPortletMode(PortletMode.VIEW);
     }
 
 }
